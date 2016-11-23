7a8c5dd9c6e5693ce0dde2503c6a20543e920a3b
Stefan Jacholke <stefanjacholke@gmail.com>
2016-11-23 22:54:16 +0200

    Send simple data type over

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 
@@ -7,6 +8,14 @@ import Control.Monad (forever)
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
 import Network.Transport.TCP (createTransport, defaultTCPParameters)
+import Data.Binary
+import Data.Typeable
+import GHC.Generics
+
+data Job = Job {processName :: String, parameters :: [String], jobId :: Int}
+  deriving (Show, Generic, Typeable)
+
+instance Binary Job
 
 replyBack :: (ProcessId, String) -> Process ()
 replyBack (sender,msg) = send sender msg
@@ -14,17 +23,21 @@ replyBack (sender,msg) = send sender msg
 logMessage :: String -> Process ()
 logMessage msg = say $ "handling " ++ msg
 
+logJob :: Job -> Process ()
+logJob job = say $ "handling " ++ show job 
+
 main :: IO ()
 main = do
   Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
   node <- newLocalNode t initRemoteTable
   _ <- runProcess node $ do
     echoPid <- spawnLocal $ forever $ do
-      receiveWait [match logMessage, match replyBack]
+      receiveWait [match logMessage, match replyBack, match logJob]
     say "send some msgs"
     send echoPid "hello echo"
     self <- getSelfPid
     send echoPid (self, "hello!")
+    send echoPid $ Job "MultiLayer" [] 0
     
     m <- expectTimeout 1000000
     case m of
