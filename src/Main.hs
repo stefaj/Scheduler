27bc78ef507b401a95717ede597e73ca14d9a499
Stefan Jacholke <stefanjacholke@gmail.com>
2016-11-23 22:30:55 +0200

    Simple demo local loopback test

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,34 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
+import Control.Distributed.Process
+import Control.Distributed.Process.Node
+import Network.Transport.TCP (createTransport, defaultTCPParameters)
+
+replyBack :: (ProcessId, String) -> Process ()
+replyBack (sender,msg) = send sender msg
+
+logMessage :: String -> Process ()
+logMessage msg = say $ "handling " ++ msg
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
+  node <- newLocalNode t initRemoteTable
+  _ <- runProcess node $ do
+    echoPid <- spawnLocal $ forever $ do
+      receiveWait [match logMessage, match replyBack]
+    say "send some msgs"
+    send echoPid "hello echo"
+    self <- getSelfPid
+    send echoPid (self, "hello!")
+    
+    m <- expectTimeout 1000000
+    case m of
+      Nothing -> die "Nothin !"
+      Just s -> say $ "got " ++ s ++ " back"
+  liftIO $ threadDelay 20000000000000
+  putStrLn "Run"
