32963c4fe0b92904c90e1a779048a3b7d1bcd894
Stefan Jacholke <stefanjacholke@gmail.com>
2016-11-24 20:02:04 +0200

    Add client state management

--- Client/src/Client.hs
+++ Client/src/Client.hs
@@ -9,6 +9,7 @@ module Client (
   where
 
 import Control.Concurrent (threadDelay, forkIO)
+import Control.Concurrent.MVar
 import Control.Monad (forever)
 -- import Control.Distributed.Process
 -- import Control.Distributed.Process.Node
@@ -22,6 +23,7 @@ import System.Timeout
 import qualified System.Process as P
 import Control.Monad 
 import Data.Maybe(catMaybes)
+import qualified Data.Sequence as S
 
 import Control.Distributed.Process
 import Control.Distributed.Process.Closure
@@ -30,9 +32,17 @@ import Control.Distributed.Process.Backend.SimpleLocalnet
 
 import Job
 
+data CurrentState = CurrentState {csStartTime :: Double
+                                 ,csCurProcHand :: P.ProcessHandle
+                                 ,csStdoutHand :: Maybe Handle
+                                 ,csJobId :: JobId
+                                 ,csJobState :: JobStatus
+                                 ,csQueue :: S.Seq Job} 
 
-startProcess :: String -> [String] -> IO String
-startProcess name args = do
+startProcess :: String -> [String] -> IO JobId
+startProcess mState name args = do
+   state <- takeMVar mState
+   let newJobId = 1 + (csJobId state)
    (_,mOut,mErr,procHandle) <- P.createProcess $ 
         (P.proc name args) { P.std_out = P.CreatePipe
                                 , P.std_err = P.CreatePipe 
@@ -40,14 +50,16 @@ startProcess name args = do
    let (hOut,hErr) = maybe (error "bogus handles") 
                            id
                            ((,) <$> mOut <*> mErr)
-   exitCode <- timeout 1000000 $ P.waitForProcess procHandle
-   sOut <- hGetContents hOut
-   P.terminateProcess procHandle
-   return sOut
+   t <- getTime
+   let queue' = csQueue S.|> (ProcessJob newJobId name args)
+   putMVar $ state {csStartTime = t, csCurProcHand = procHandle, 
+                   ,csStdoutHand = hOut, csJobId = newJobId,
+                   ,csQueue = queue'}
+   return newJobId
 
-handleStartProcess backend (StartProcess name args) = do
-  res <- liftIO $ startProcess name args
-  sendMaster backend res
+handleStartProcess mState backend (StartProcess name args) = do
+  newId <- liftIO $ startProcess name args
+  sendMaster backend $ StartRes newId
 
 logSlaveMessage :: String -> Process ()
 logSlaveMessage msg = say $ "Slave: handling " ++ msg
@@ -71,10 +83,29 @@ findMaster backend = do
        ])
 
 slave backend = do
+  liftIO $ initializeTime
   pid <- getSelfPid
   register "slaveController" pid
+  mState <- liftIO $ newMVar $ CurrentState 0 0 Nothing 0 S.empty
   forever $ do
     liftIO $ putStrLn $ "Waiting for message"
-    receiveWait ([match logSlaveMessage, match (handleStartProcess backend) ])
+    receiveWait ([match logSlaveMessage, match (handleStartProcess mState backend) ])
     liftIO $ putStrLn $ "Waiting for next cycle"
     liftIO $ threadDelay 100000
+
+getAvailableStdOut :: Process -> IO T.Text
+getAvailableStdOut (Process _ _ _ _ (Just (ProcessData o _ _ _))) = do
+  contents <- hGetAvailableContents o
+  return $ T.pack contents
+getAvailableStdOut Process{} = return $ T.pack ""
+
+hGetAvailableContents :: Handle -> IO String
+hGetAvailableContents = flip hGetAvailableContents' []
+hGetAvailableContents' :: Handle -> String -> IO String
+hGetAvailableContents' h buff = do
+  r <- hReady h
+  if r then do
+    c <- hGetChar h
+    hGetAvailableContents' h (c:buff)
+  else
+    return $ reverse buff
